a19ea935c4df1239ef36bd212a82c850e47b9229
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-14 18:44:43 +0100

    defined the variables word and added the function to verify if the inverted word is equal to the original word

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+let word = prompt('Inserisci una parola');
+
+function palindromcheck(string){
+    if(string.toLowerCase().split('').reverse().join('') === string){
+        return true
+        console.log('è palindroma');
+    }
+}
+let risultato = palindromcheck(word.toLowerCase());
+
+if(risultato){
+    console.log('la parola è palindroma');
+}
+else console.log('la parola non è palindroma');
+
+
 
 /*Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
